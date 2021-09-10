80f19ed2bdafc0392b27e631de62024a672cd07e
maosan132 <maosan132@gmail.com>
2021-09-10 17:13:00 -0500

    Set state to navigatio comp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navigation = () => (
+  const showMenu = [showMenu, setShowMenu] = useState(false);
   <nav>
     <span className="text-xl">
       <FontAwesomeIcon icon={faBars} />
