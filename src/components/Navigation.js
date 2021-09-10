eb8890524d394c6804b7ef11779c3019d2615ec2
maosan132 <maosan132@gmail.com>
2021-09-10 18:29:40 -0500

    Add the menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-const Navigation = () => (
-  const showMenu = [showMenu, setShowMenu] = useState(false);
-  <nav>
+const Navigation = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  let menu
+  let menuMask
+
+  if (showMenu) {
+    menu =
+    <div className="fixed bg-white top-0 left-0 w-4/5 h-full shadow">
+      menu here
+    </div>
+    menuMask =
+    <div className="bg-black fixed top-0 left-0 w-full h-full">
+      menu here
+    </div>
+  }
+
+  return (
+    <nav>
     <span className="text-xl">
-      <FontAwesomeIcon icon={faBars} />
+      <FontAwesomeIcon
+      icon={faBars}
+      onClick={() => setShowMenu(!showMenu) }/>
     </span>
+    {menuMask}
+    {menu}
   </nav>
-)
+    )
+
+}
+
 
 export default Navigation;
