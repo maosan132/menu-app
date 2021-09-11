abfcdd961ed7d867227fbd68efe033f8c0a9bbf2
maosan132 <maosan132@gmail.com>
2021-09-11 17:25:34 -0500

    Connect API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import HelloWorld from './components/helloWorld';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './views/About';
 import Home from './views/Home';
 import Product from './views/Product'
--- src/views/Product.js
+++ src/views/Product.js
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function Product() {
-  return (
-    <div>
-      <h1 className="font-bold text-2xl">A Product</h1>
-      <p>
-        Content for Product page...
-      </p>
-    </div>
+  const url = 'https://613cd4e5270b96001798b299.mockapi.io/api/v1/test/3';
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setProduct(response.data)
+    })
+  }, [url])
+
+
+    if (product) {
+      return (
+        <div>
+          <h1 className="font-bold text-2xl">{product.name}</h1>
+          <p>
+            Content for Product page...
+          </p>
+        </div>
+      )
+    }
+
+    return (
+      <div></div>
   )
 }
