e9fe9e3170bda22733823dc9caba9d39d49b982d
maosan132 <maosan132@gmail.com>
2021-09-10 20:03:50 -0500

    Install react-spring with --save

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,26 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { useTransition, animated } from 'react-spring'
 
 const Navigation = () => {
   const [showMenu, setShowMenu] = useState(false);
 
-  let menu
-  let menuMask
+  // let menu
+  // let menuMask
 
-  if (showMenu) {
-    menu =
-    <div className="fixed bg-white top-0 left-0 w-4/5 h-full shadow">
-      menu here
-    </div>
-    menuMask =
-    <div className="fixed top-0 left-0 w-full h-full bg-black-t-50 z-"
-      onClick={() => setShowMenu(false)}
-    >
+  // if (showMenu) {
+  //   menu =
+  //   <div className="fixed bg-white top-0 left-0 w-4/5 h-full shadow">
+  //     menu here
+  //   </div>
+  //   menuMask =
+  //   <div className="fixed top-0 left-0 w-full h-full bg-black-t-50 z-"
+  //     onClick={() => setShowMenu(false)}
+  //   >
       
-    </div>
-  }
+  //   </div>
+  // }
 
   return (
     <nav>
