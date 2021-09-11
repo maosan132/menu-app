b83af8d0c356fa138d4159889f31b07daf46d2b0
maosan132 <maosan132@gmail.com>
2021-09-11 17:35:29 -0500

    Fill product page with rest of content from API

--- src/views/Product.js
+++ src/views/Product.js
@@ -5,6 +5,8 @@ export default function Product() {
   const url = 'https://613cd4e5270b96001798b299.mockapi.io/api/v1/test/3';
   const [product, setProduct] = useState(null);
 
+  let content = null
+
   useEffect(() => {
     axios.get(url)
     .then(response => {
@@ -14,17 +16,30 @@ export default function Product() {
 
 
     if (product) {
-      return (
-        <div>
-          <h1 className="font-bold text-2xl">{product.name}</h1>
-          <p>
-            Content for Product page...
-          </p>
+      // return (
+      //   <div>
+      //     <h1 className="font-bold text-2xl">{product.name}</h1>
+      //     <p>
+      //       Content for Product page...
+      //     </p>
+      //   </div>
+      // )}
+      content =
+      <div>
+        <h1 className="font-bold text-2xl mb-3">{product.name}</h1>
+        <img src={product.images[0].imageUrl} alt="{product.name}" />
+        <div className="font-bold text-xl mb-3 border text-center">
+          ${product.price}
         </div>
-      )
+        <p>
+          {product.description}
+        </p>
+      </div>
     }
 
     return (
-      <div></div>
+      <div>
+        {content}
+      </div>
   )
 }
