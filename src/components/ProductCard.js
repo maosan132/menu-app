98c03bc38e3d5b24fae47a356f5999883e80b2cc
maosan132 <maosan132@gmail.com>
2021-09-11 23:17:13 -0500

    After adding a list of product names to home, implement productCard comp

---
+++ src/components/ProductCard.js
@@ -0,0 +1,7 @@
+const ProductCard = () => (
+  <div>
+    
+  </div>
+)
+
+export default ProductCard;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,31 +1,32 @@
 // import HelloWorld from '../components/helloWorld'
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Loader from '../components/Loader';
 
 const Home = () => {
   const url = `https://613cd4e5270b96001798b299.mockapi.io/api/v1/test?page=1&limit=10`;
-  const [product, setProduct] = useState({
+  const [products, setProducts] = useState({
     data: null,
     loading: false,
     error: false
   });
 
   useEffect(() => {
-    setProduct({
+    setProducts({
       loading: true,
       data: null,
       error: false
     })
     axios.get(url)
     .then(response => {
-      setProduct({
+      setProducts({
         loading: false,
         data: response.data,
         error: false
       })
     })
     .catch(() => {
-      setProduct({
+      setProducts({
         loading: false,
         data: null,
         error: true
@@ -34,6 +35,24 @@ const Home = () => {
   }, [url]);
 
   let content = null;
+
+  
+  if (products.error) {
+    content = <p>There was an error. Please refresh or try again later.</p>
+  }
+
+  if (products.loading) {
+    content = <Loader />
+  }
+
+  if (products.data) {
+    content =
+    products.data.map((p,k) =>
+      <div>
+        {p.name}
+      </div>
+    )
+  }
 
   return (
     <div>
