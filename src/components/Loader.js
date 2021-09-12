da0480a206939b13c1ed616101303dde59aa8139
maosan132 <maosan132@gmail.com>
2021-09-11 19:17:29 -0500

    Implement loader comp5

---
+++ src/components/Loader.js
@@ -0,0 +1,8 @@
+const Loader = () => (
+  <div className="flex justify-center">
+    <div className="loader">
+    </div>
+  </div>
+)
+
+return default Loader;
--- src/views/Product.js
+++ src/views/Product.js
@@ -5,12 +5,20 @@ import { useParams} from 'react-router-dom';
 export default function Product() {
   const { id } = useParams();
   const url = `https://613cd4e5270b96001798b299.mockapi.io/api/v1/test/${id}`;
+  // const [product, setProduct] = useState(null);
   const [product, setProduct] = useState({
     data: null,
     loading: false,
   });
 
-  let content = null
+  let content = null;
+
+  // useEffect(() => {
+  //   axios.get(url)
+  //   .then(response => {
+  //     setProduct(response.data);
+  //   })
+  // }, [url])
 
   useEffect(() => {
     setProduct({
@@ -26,32 +34,35 @@ export default function Product() {
     })
   }, [url])
 
+  if (product.loading) {
+    content = <div>Loading...</div>
+  }
 
-    if (product.data) {
-      // return (
-      //   <div>
-      //     <h1 className="font-bold text-2xl">{product.data.name}</h1>
-      //     <p>
-      //       Content for Product page...
-      //     </p>
-      //   </div>
-      // )}
-      content =
-      <div>
-        <h1 className="font-bold text-2xl mb-3">{product.data.name}</h1>
-        <img src={product.data.images[0].imageUrl} alt="{product.data.name}" />
-        <div className="font-bold text-xl mb-3 border text-center">
-          ${product.data.price}
-        </div>
-        <p>
-          {product.data.description}
-        </p>
+  if (product.data) {
+    // return (
+    //   <div>
+    //     <h1 className="font-bold text-2xl">{product.data.name}</h1>
+    //     <p>
+    //       Content for Product page...
+    //     </p>
+    //   </div>
+    // )}
+    content =
+    <div>
+      <h1 className="font-bold text-2xl mb-3">{product.data.name}</h1>
+      <img src={product.data.images[0].imageUrl} alt="{product.data.name}" />
+      <div className="font-bold text-xl mb-3 border text-center">
+        ${product.data.price}
       </div>
-    }
+      <p>
+        {product.data.description}
+      </p>
+    </div>
+  }
 
-    return (
-      <div>
-        {content}
-      </div>
+  return (
+    <div>
+      {content}
+    </div>
   )
 }
