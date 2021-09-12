71c97b013343eba607bc6693fa5545af184204a6
maosan132 <maosan132@gmail.com>
2021-09-11 23:07:29 -0500

    Insert axios to home comp and change url to a list of 10 products with ?page=1&limit=10

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,38 @@
-import HelloWorld from '../components/helloWorld'
+// import HelloWorld from '../components/helloWorld'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const url = `https://613cd4e5270b96001798b299.mockapi.io/api/v1/test?page=1&limit=10`;
+  const [product, setProduct] = useState({
+    data: null,
+    loading: false,
+    error: false
+  });
+
+  useEffect(() => {
+    setProduct({
+      loading: true,
+      data: null,
+      error: false
+    })
+    axios.get(url)
+    .then(response => {
+      setProduct({
+        loading: false,
+        data: response.data,
+        error: false
+      })
+    })
+    .catch(() => {
+      setProduct({
+        loading: false,
+        data: null,
+        error: true
+      })
+    })
+  }, [url]);
+
   let content = null;
 
   return (
--- src/views/Product.js
+++ src/views/Product.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Loader from '../components/Loader'
 
 export default function Product() {
